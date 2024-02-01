6280f3b9625a2d3931e1f940f42cc86ab7a60548
karthikpalanisamyRIPIT <158033099+karthikpalanisamyRIPIT@users.noreply.github.com>
2024-02-01 10:22:05 +0530

    Add files via upload

---
+++ LanguageDetailss/src/org/all/LanguageClass.java
@@ -0,0 +1,21 @@
+package org.all;
+
+import org.telugu.Telugu;
+
+public class LanguageClass extends Telugu{
+	public static void main(String[] args) {
+		Telugu c=new Telugu();
+		
+		c.tamillanguage();
+		c.englishlanguage();
+		c.telugulanguage();
+		
+		
+	}
+	private void alllanguage() {
+
+
+
+	}
+
+}
---
+++ LanguageDetailss/src/org/telugu/Telugu.java
@@ -0,0 +1,11 @@
+package org.telugu;
+
+import org.english.English;
+;
+
+public class Telugu extends English{
+public void telugulanguage() {
+System.out.println("Telugu only");
+
+}
+}
